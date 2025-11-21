823a0e097125c9624620ca00ee3328b4ee13fc3c
ANAS BENRJAB <benrejebanas8@gmail.com>
2025-11-21 14:06:40 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', component: ListSuggestionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
